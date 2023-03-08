1d0289b956ab542a2b34ba3b28bb8232b33ca56e
Harshithasri2004 <126843485+Harshithasri2004@users.noreply.github.com>
2023-03-08 09:24:09 +0530

    Create bubblesort.c

---
+++ bubblesort.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int arr[20],size,i,j,temp;
+    printf("Enter the size of array");
+    scanf("%d",&size);
+    printf("Enter the elements of the array,,,,,");
+    for(i=0;i<size;i++)
+    {
+        printf("\n Enter the %d element",i+1);
+        scanf("%d", &arr[i]);
+    }
+    if(i=0,i<size)
+    printf("/n sorted array is,,,,,,");
+    for(i=0;i<size;i++)
+    printf("%d",arr[i]);
+    return 0;
+}
+
+*/
+  output;
+  Enter the size of array 4
+4
+Enter the elements of the array,,,,,
+ Enter the 1 element1
+ Enter the 2 element2
+ Enter the 3 element3
+ Enter the 4 element4
+ /n sorted array is,,,,,,1234
+   */
